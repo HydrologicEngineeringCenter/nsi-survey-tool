8c63f71c5e92edd9772e6d40cbb37c467434fddf
Will Lehman <william.p.lehman@usace.army.mil>
2020-10-14 06:43:22 -0700

    updating the banner

--- src/containers/main/banner.js
+++ src/containers/main/banner.js
@@ -7,7 +7,7 @@ class Banner extends React.Component{
             <nav className="navbar navbar=expand-lg navbar-dark bg-dark">
                 <div className="float-left">
                     <a href = "/"><img src={mapper} alt="USACE LOGO" style={{width:'45px'}}/></a>
-                    <a className="navbar-brand" href="/" style={{paddingLeft:"15px", fontSize:'25px'}}><b>@corpsmap</b></a>           
+                    <a className="navbar-brand" href="/" style={{paddingLeft:"15px", fontSize:'25px'}}><b>NSI Survey Tool</b></a>           
                 </div>
             </nav>
         )
