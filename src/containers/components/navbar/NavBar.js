b7eb7fb72468a6721ab67725e93e061bfb8f95bd
Triet Nguyen <triet.nguyen@usace.army.mil>
2021-09-09 21:23:52 +0000

    added cwbi logo, rm extra assets, centered nsi logo

--- src/containers/components/navbar/NavBar.js
+++ src/containers/components/navbar/NavBar.js
@@ -3,25 +3,31 @@ import { connect } from 'redux-bundler-react';
 import mapper from '../../../resources/GeoFDA_Icon.ico';
 import '../../main/main.css';
 import classes from './NavBar.module.css';
+import cwbiLogo from "../../../resources/CWBI_Logo_45x45.png";
 
+function NavBar(props) {
 
-function NavBar(props){
+  const { authUserName: userName } = props;
 
-    const{authUserName:userName} = props;
+  return (
+    <nav className={`navbar navbar=expand-lg navbar-dark nv-bg`}>
 
-    return (
-        <nav className={`navbar navbar=expand-lg navbar-dark nv-bg`}>
-            <a href="/nsi-survey" className={`${classes['float-left']} "float-left"`}>
-                <img src={mapper} alt="NSI LOGO" style={{width:'35px'}}/>
-                <a className={classes['navbar-brand']}>NSI Survey Tool</a>           
-            </a>
-            <div>
-                {userName}
-            </div>
-        </nav>
-    )
+      <div className={`${classes['float-left']} "nav float-left"`}>
+        <a href="/nsi-survey"><img src={mapper} alt="NSI LOGO" style={{ width: '45px' }} /> </a>
+        <a className='navbar-brand' href="/nsi-survey" style={{ paddingLeft: "15px", fontSize: '25px' }}><b>NSI Survey Tool</b></a>
+      </div>
+
+      <div className="nav pull-right">
+        <div className={`${classes['flex-ac']} ${classes['user-name']}`}>
+          {userName}
+        </div>
+        <img className={classes["cwbi-logo"]} title="Powered By Civil Works Business Intelligence" src={cwbiLogo} alt="CWBI LOGO" />
+      </div>
+
+    </nav>
+  )
 }
 export default connect(
-    'selectAuthUserName',
-    NavBar
+  'selectAuthUserName',
+  NavBar
 );
--- src/containers/create-new-survey/form-components/ButtonHelp.jsx
+++ src/containers/create-new-survey/form-components/ButtonHelp.jsx
@@ -1,9 +1,5 @@
-import SelectButton from "../../components/SelectButton/SelectButton";
-import HelpSvg from "../../../resources/survey-splash/help_outline_black_24dp.svg";
 import classes from "./ButtonHelp.module.css";
 import Button from "@material-ui/core/Button";
-import { Fragment } from "react";
-import Card from "../../components/UI/Card";
 
 const ButtonHelp = (props) => {
   const { children } = props;
