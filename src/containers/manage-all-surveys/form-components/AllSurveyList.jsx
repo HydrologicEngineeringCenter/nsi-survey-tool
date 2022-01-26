cfcf6c285cd0fb49d12f41ffe9c536f9d61386d8
TMNT <trietpersonal@gmail.com>
2022-01-26 12:50:52 -0500

    remove sticky table header from main survey list in manage-all-surveys

--- src/containers/manage-all-surveys/form-components/AllSurveyList.jsx
+++ src/containers/manage-all-surveys/form-components/AllSurveyList.jsx
@@ -41,7 +41,7 @@ const AllSurveyList = (props) => {
 
   return (
     <TableContainer component={Paper} className={classes["active-survey-list"]}>
-      <Table className={classes.table} stickyHeader arial-label="simple-table">
+      <Table className={classes.table} arial-label="simple-table">
 
         <TableHead>
           <TableRow>
