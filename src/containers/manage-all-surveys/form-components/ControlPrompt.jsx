9eec02a7a0487af1f4e8d6c7ef54a682c71d8d74
TMNT <trietpersonal@gmail.com>
2022-01-27 16:37:35 -0500

    added tabs to control prompt

--- src/containers/manage-all-surveys/form-components/ControlPrompt.jsx
+++ src/containers/manage-all-surveys/form-components/ControlPrompt.jsx
@@ -1,4 +1,4 @@
-import { useEffect, Fragment } from "react"
+import { useState, Fragment } from "react"
 import { connect } from "redux-bundler-react"
 import Box from "@material-ui/core/Box"
 import Typography from "@material-ui/core/Typography"
@@ -7,6 +7,10 @@ import ActiveSurveySwitch from "./ActiveSurveySwitch/ActiveSurveySwitch"
 import AsyncSurveyorAutocomplete from "./../../components/SurveyorPrompt/AsyncSurveyorAutocomplete"
 import SurveyorsList from "./../../components/SurveyorPrompt/SurveyorsList"
 import SelectButton from "./../../components/SelectButton/SelectButton"
+import Tabs from "@material-ui/core/Tabs"
+import Tab from "@material-ui/core/Tab"
+import TabPanel from "./TabPanel"
+import AppBar from "@material-ui/core/AppBar"
 import classes from "./ControlPrompt.module.css"
 
 // TODO customize css
@@ -19,35 +23,64 @@ const ControlPrompt = props => {
     doSurvey_downloadReport,
   } = props
 
+  const [value, setValue] = useState(0)
+  const handleChange = (_, newValue) => {
+    setValue(newValue)
+  }
+  const a11yProps = idx => {
+    return {
+      id: `scrollable-auto-tab-${idx}`,
+      'aria-controls': `scrollable-auto-tabpanel-${idx}`,
+    }
+  }
+
   return (
     <Fragment>
-      <Box sx={{ margin: 1 }}>
-        <Card className={"surveyors-container"}>
-          <Typography variant="h6" gutterBottom component="div">
-            Edit Survey
-            <ActiveSurveySwitch initActive={initActive} />
-            <p />
-            <AsyncSurveyorAutocomplete />
-            <p />
-            <SelectButton onClick={_ =>
-              doUser_sendRequestAddSurveyor(
-                survey_selectedSurvey.id,
-                user_selectedUser.userId
-              )}>Add Surveyors
-            </SelectButton>
-            <p />
-            <p />
-            <SurveyorsList showDelete={true} />
+      <Card className={"surveyors-container"}>
 
-            <p />
-            <p />
-            <SelectButton onClick={doSurvey_downloadReport}>Download</SelectButton>
-            <p />
-            <p />
+        <Typography variant="h6" gutterBottom component="div">
+          Edit Survey
+        </Typography>
 
-          </Typography>
-        </Card>
-      </Box>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="scrollable"
+            scrollButtons="auto"
+            aria-label="scrollable auto tabs example"
+          >
+            <Tab label={survey_selectedSurvey ? survey_selectedSurvey.id : ""} {...a11yProps(0)} />
+            <Tab label="Change Surveyors" {...a11yProps(1)} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0}>
+          <ActiveSurveySwitch initActive={initActive} />
+          <p />
+          <p />
+          <SelectButton onClick={doSurvey_downloadReport}>Download</SelectButton>
+          <p />
+          <p />
+        </TabPanel>
+
+        <TabPanel value={value} index={1}>
+          <AsyncSurveyorAutocomplete />
+          <p />
+          <p />
+          <SelectButton onClick={_ =>
+            doUser_sendRequestAddSurveyor(
+              survey_selectedSurvey.id,
+              user_selectedUser.userId
+            )}>Add Surveyors
+          </SelectButton>
+          <p />
+          <p />
+          <SurveyorsList showDelete={true} />
+        </TabPanel>
+
+      </Card>
     </Fragment >
   )
 }
---
+++ src/containers/manage-all-surveys/form-components/TabPanel.jsx
@@ -0,0 +1,22 @@
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
+export default props => {
+
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  )
+}
